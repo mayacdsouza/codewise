7684952d23609c675b2b1b778ff4e4a64e73d587
Maya D'Souza <dsouzam@oregonstate.edu>
2023-07-20 18:40:20 -0500

    In progress

--- src/components/Surveys.jsx
+++ src/components/Surveys.jsx
@@ -6,6 +6,22 @@ import { Link } from "react-router-dom";
 const Surveys = () => {
   // Todo: Existing surveys should dynamically display created surveys instead of being hardcoded.
   // Todo: Send to candidate should auto-generate a key link for that survey and email to the candidate. Should also create a results entry connected to that.
+
+  fetch("localhost:3306/surveys")
+    .then((response) => {
+      if (response.ok) {
+        return response;
+      }
+      throw new Error("Something went wrong");
+    })
+    .then((data) => {
+      return data.json();
+    })
+    .then((result) => console.log(result))
+    .catch((error) => {
+      console.log(error);
+    });
+
   return (
     <div className="settings-container">
       <h1>Surveys</h1>
