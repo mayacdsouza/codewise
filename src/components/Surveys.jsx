d9da47ae0e85e68bc9eb743eb1cbf003e2a416ef
Maya D'Souza <dsouzam@oregonstate.edu>
2023-07-20 23:09:28 -0500

    improved flow of useeffect hooks

--- src/components/Surveys.jsx
+++ src/components/Surveys.jsx
@@ -6,94 +6,92 @@ const Surveys = () => {
   // Todo: Change candidateName and email to be unique for each input element
   // Todo: Send to candidate should auto-generate a key link for that survey and email to the candidate. Should also create a results entry connected to that.
 
-  const [candidateName, setCandidateName] = useState();
-  const [candidateEmail, setCandidateEmail] = useState();
-
-  const handleSendToCandidate = () => {
-    // Generate a random key link
-    // Todo: Key link does not link to anything at the moment
-    const keyLink = Math.random().toString(36).substring(7);
-
-    // Prepare the email subject and body
-    const subject = "Your Survey Link";
-    const body = `Dear ${candidateName},\n\nPlease click the following link to access your survey: ${keyLink}\n\nBest regards,\nCodewise`;
-
-    // Generate the mailto link
-    const mailtoLink = `mailto:${candidateEmail}?subject=${encodeURIComponent(
-      subject
-    )}&body=${encodeURIComponent(body)}`;
-
-    // Open the default email client with the pre-filled email fields
-    window.location.href = mailtoLink;
+  const handleCandidateInput = (e) => {
+    e.preventdefault();
+    console.log("Submitted");
   };
+
+  // const [candidateName, setCandidateName] = useState();
+  // const [candidateEmail, setCandidateEmail] = useState();
+
+  // const handleSendToCandidate = () => {
+  //   // Generate a random key link
+  //   // Todo: Key link does not link to anything at the moment
+  //   const keyLink = Math.random().toString(36).substring(7);
+
+  //   // Prepare the email subject and body
+  //   const subject = "Your Survey Link";
+  //   const body = `Dear ${candidateName},\n\nPlease click the following link to access your survey: ${keyLink}\n\nBest regards,\nCodewise`;
+
+  //   // Generate the mailto link
+  //   const mailtoLink = `mailto:${candidateEmail}?subject=${encodeURIComponent(
+  //     subject
+  //   )}&body=${encodeURIComponent(body)}`;
+
+  //   // Open the default email client with the pre-filled email fields
+  //   window.location.href = mailtoLink;
+  // };
 
   const [quizzes, setQuizzes] = useState();
   const [tableEntries, setTableEntries] = useState();
   const [options, setOptions] = useState();
   const [candidates, setCandidates] = useState();
 
-  async function fetchQuizzes() {
-    const data = await fetch("http://localhost:3306/select_quizzes");
-    return data;
-  }
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
 
-  async function fetchCandidates() {
-    const data = await fetch("http://localhost:3306/select_candidates");
-    return data;
-  }
+    async function fetchQuizzes() {
+      const response = await fetch("http://localhost:3306/select_quizzes", {
+        signal,
+      });
+      const data = await response.json();
+      setQuizzes(data);
+      setTableEntries(
+        quizzes?.map(function (element) {
+          return (
+            <tr key={element.id} employee-id={element.Employers_id}>
+              <td>{element.title}</td>
+              <td>
+                <select name="candidates" id="candidates">
+                  {options}
+                </select>
+              </td>
+              <td>
+                <button className="btn">Send to candidate</button>
+              </td>
+            </tr>
+          );
+        })
+      );
+    }
+    fetchQuizzes();
+  }, [quizzes]);
 
   useEffect(() => {
-    fetchQuizzes()
-      .then((data) => {
-        return data.json();
-      })
-      .then((result) => {
-        setQuizzes(result);
-        setTableEntries(
-          quizzes?.map(function (element) {
-            return (
-              <tr key={element.id} employee-id={element.Employers_id}>
-                <td>{element.title}</td>
-                <td>
-                  <select name="candidates" id="candidates">
-                    {options}
-                  </select>
-                </td>
-                <td>
-                  <button className="btn" onClick={handleSendToCandidate}>
-                    Send to candidate
-                  </button>
-                </td>
-              </tr>
-            );
-          })
-        );
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [fetchQuizzes]);
+    const controller = new AbortController();
+    const signal = controller.signal;
 
-  useEffect(() => {
-    fetchCandidates()
-      .then((data) => {
-        return data.json();
-      })
-      .then((result) => {
-        setCandidates(result);
-        setOptions(
-          candidates?.map(function (element) {
-            return (
-              <option id={element.id} value={element.name}>
-                {element.name}
-              </option>
-            );
-          })
-        );
-      })
-      .catch((error) => {
-        console.log(error);
+    async function fetchCandidates() {
+      const response = await fetch("http://localhost:3306/select_candidates", {
+        signal,
       });
+      const data = await response.json();
+      setCandidates(data);
+      setOptions(
+        candidates?.map(function (element) {
+          return (
+            <option key={element.id} value={element.name}>
+              {element.name}
+            </option>
+          );
+        })
+      );
+      return () => {
+        controller.abort();
+      };
+    }
+    fetchCandidates();
   }, [candidates]);
   return (
     <div className="settings-container">
@@ -110,9 +108,17 @@ const Surveys = () => {
       <div className="profile-edit">
         {" "}
         <h2>Add a new candidate</h2>
-        Name <input />
-        Email <input />
-        <button>Submit</button>
+        <form onSubmit={handleCandidateInput}>
+          <label>
+            Name:
+            <input type="text" name="name" placeholder="Name" />
+          </label>
+          <label>
+            Email
+            <input type="text" name="email" placeholder="Email" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <br></br>
         <br></br>
         <br></br>
